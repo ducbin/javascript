72a9064c2239b849b45299ef0e858a67b04d206b
ducbin <vuducbinhster@gmail.com>
2012-12-06 12:55:39 +0100

    Update iscroll.js

--- iscroll.js
+++ iscroll.js
@@ -1,6 +1,7 @@
 /*!
  * iScroll v4.1.9 ~ Copyright (c) 2011 Matteo Spinelli, http://cubiq.org
- * Released under MIT license, http://cubiq.org/license
+ * Released under MIT license
+ * http://cubiq.org/license
  */
 (function(){
 var m = Math,
